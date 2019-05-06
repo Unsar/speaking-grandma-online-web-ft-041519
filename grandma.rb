a6dc81fa6f4d6e32a09f3ec2ede898811d7d2bcb
Unsar Malik <unsar.malik24@gmail.com>
2019-05-06 19:27:31 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma(phrase)
   phrase = "HUH?! SPEAK UP, SONNY!"
   
-  if phrase.upcase
+  if phrase = phrase.upcase
     puts "NO, NOT SINCE 1938"
   
   elsif phrase == "I LOVE YOU GRANDMA!"
