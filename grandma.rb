af9cb0befb4f69e6ac27eb6035bef93da90de31a
Unsar Malik <unsar.malik24@gmail.com>
2019-05-06 19:30:59 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,10 +16,11 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
     
-  elsif phrase 
+  elsif phrase==phrase.downcase
     return "HUH?! SPEAK UP, SONNY!"
     
-  else 
+  else phrase==phrase.upcase
+    return 
   end
 end
   
