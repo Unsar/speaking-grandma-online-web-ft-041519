739db34f0e1721d5bbaf63880f6b4504e6542e86
Unsar Malik <unsar.malik24@gmail.com>
2019-05-06 19:30:16 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,14 +13,13 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if phrase = phrase.upcase
-    return "NO, NOT SINCE 1938!"
-  
-  elsif phrase == "I LOVE YOU GRANDMA!"
+  if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
     
-  else 
+  elsif phrase 
     return "HUH?! SPEAK UP, SONNY!"
+    
+  else 
   end
 end
   
